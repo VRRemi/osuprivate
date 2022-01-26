a1f4cc3381cd481b13f749af2ed017014a52145d
VRRemi <business@vrremi.xyz>
2022-01-26 11:15:52 -0800

    Update process.hpp

--- osuprivate/process.hpp
+++ osuprivate/process.hpp
@@ -3,3 +3,9 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <vector>
+
+namespace process {
+	DWORD getProcessId(const wchar_t* processName);
+	uintptr_t getModuleBaseAddress(DWORD processId, const wchar_t* moduleName);
+	uintptr_t getPointer(HANDLE hProcess, uintptr_t pointer, std::vector<unsigned int> offsets);
+}
