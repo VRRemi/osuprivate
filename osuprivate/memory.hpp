4764d15d253962a2fadf9cbcecd321a0bca97239
VRRemi <business@vrremi.xyz>
2022-01-26 11:12:39 -0800

    Update memory.hpp

--- osuprivate/memory.hpp
+++ osuprivate/memory.hpp
@@ -2,3 +2,8 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <iostream>
+namespace memory {
+	void PatchMemory(BYTE* destination, BYTE* source, unsigned int size, HANDLE hProcess);
+	void NopMemory(BYTE* destination, unsigned int size, HANDLE hProcess);
+	void enable_debug_privilege();
+}
