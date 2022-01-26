18b867d4b908dfbff9d24ffef097647909394f1f
VRRemi <business@vrremi.xyz>
2022-01-26 11:13:26 -0800

    Update memory.cpp

--- osuprivate/memory.cpp
+++ osuprivate/memory.cpp
@@ -1,4 +1,8 @@
 #include "memory.hpp"
 
 void memory::PatchMemory(BYTE* destination, BYTE* source, unsigned int size, HANDLE hProcess) {
+	DWORD oldProtect;
+	VirtualProtectEx(hProcess, destination, size, PAGE_EXECUTE_READWRITE, &oldProtect);
+	WriteProcessMemory(hProcess, destination, source, size, nullptr);
+	VirtualProtectEx(hProcess, destination, size, oldProtect, &oldProtect);
 }
