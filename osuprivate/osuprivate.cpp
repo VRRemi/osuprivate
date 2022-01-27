e4761d9135b4002515454a50e5aa84a2bfd69e5b
VRRemi <business@vrremi.xyz>
2022-01-27 09:14:02 -0800

    Update osuprivate.cpp

---
+++ osuprivate/osuprivate.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include <limits>
+#include <TlHelp32.h>
+#include <cstdint>
+#include "memory.hpp"
+#include "process.hpp"
+#include "initialize.hpp"
+#include "sigs.hpp"
+#include "writereadFunc.hpp"
+#include "hacks.hpp"
+#include <stdlib.h>
