8fc1edc40d4312a815b8de5eaf7b76e67130865f
VRRemi <business@vrremi.xyz>
2022-01-27 08:57:24 -0800

    Update process.cpp

--- osuprivate/process.cpp
+++ osuprivate/process.cpp
@@ -3,6 +3,8 @@
 DWORD process::getProcessId(const wchar_t* processName) {
 	DWORD procId{ 0 };
 	HANDLE hSnap{ CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0) };
+	if (hSnap != INVALID_HANDLE_VALUE) {
+		PROCESSENTRY32 procEntry;
 				}
 
 }
