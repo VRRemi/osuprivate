3bd0a16016c106220137490e05eb247045b318f7
VRRemi <business@vrremi.xyz>
2022-01-27 09:04:31 -0800

    Update hacks.hpp

--- osuprivate/hacks.hpp
+++ osuprivate/hacks.hpp
@@ -1,4 +1,8 @@
 #pragma once
 #include "memory.hpp"
 namespace hacks {
+	void enableScoreSub(uintptr_t address, HANDLE hProcess);
+	void disableScoreSub(uintptr_t address, HANDLE hProcess);
+	void enableTaikoManiaBypass(uintptr_t address, HANDLE hProcess);
+	//void disableTaikoManiaBypass(uintptr_t address,  HANDLE hProcess);
 }
