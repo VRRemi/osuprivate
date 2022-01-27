eab95e584d900180d7e58624a8e6e4bc34df4627
VRRemi <business@vrremi.xyz>
2022-01-27 08:58:22 -0800

    Update sigs.cpp

--- osuprivate/sigs.cpp
+++ osuprivate/sigs.cpp
@@ -1,1 +1,6 @@
 #include "sigs.hpp"
+uintptr_t sigs::findPattern(HANDLE hProcess, const unsigned char pattern[], const char* mask, const int offset) {
+	const size_t signature_size = strlen(mask);
+	const size_t read_size = 4096;
+	const size_t beginAddress = 0x04000000;
+	bool isFound = false;
