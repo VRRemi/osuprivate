aa15e1622af1146fe43a3dbed18e70ebd4d83fe0
VRRemi <business@vrremi.xyz>
2022-01-27 09:01:44 -0800

    Update initialize.hpp

--- osuprivate/initialize.hpp
+++ osuprivate/initialize.hpp
@@ -9,3 +9,6 @@ namespace init {
 	std::string random_string(const int length) {
 		const std::string alpha_numeric("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890");
 
+		std::default_random_engine generator{ std::random_device{}() };
+		const std::uniform_int_distribution< std::string::size_type > distribution{ 0, alpha_numeric.size() - 1 };
+
