bd54f8d1bb8fd20ed128b795ac709b0bc7291bd4
VRRemi <business@vrremi.xyz>
2022-01-27 09:00:10 -0800

    Update writereadFunc.hpp

--- osuprivate/writereadFunc.hpp
+++ osuprivate/writereadFunc.hpp
@@ -1,2 +1,7 @@
 #pragma once
 #include "memory.hpp"
+
+template <typename T>
+bool writeMem(uintptr_t address, T value, HANDLE hProcess) {
+	return WriteProcessMemory(hProcess, (LPVOID)(address), &value, sizeof(value), nullptr);
+}
