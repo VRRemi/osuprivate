44c0f64648fc6d26a9301c6f48f1549df1dea2e0
VRRemi <business@vrremi.xyz>
2022-01-27 09:04:46 -0800

    Update hacks.cpp

--- osuprivate/hacks.cpp
+++ osuprivate/hacks.cpp
@@ -3,3 +3,7 @@ void hacks::enableScoreSub(uintptr_t address, HANDLE hProcess) {
 	memory::PatchMemory((BYTE*)address, (BYTE*)"\x55", 1, hProcess);
 }
 
+void hacks::disableScoreSub(uintptr_t address, HANDLE hProcess) {
+	memory::PatchMemory((BYTE*)address, (BYTE*)"\xC3", 1, hProcess);
+}
+
